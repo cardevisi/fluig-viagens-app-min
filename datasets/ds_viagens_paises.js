07ad9777b054d2be72f0709b4f86f0f55ddd15f0
Carlos A S Oliveira <carlos.aoliveira@totvs.com.br>
2026-07-03 16:05:38 -0300

    debug: update datasets

--- datasets/ds_viagens_paises.js
+++ datasets/ds_viagens_paises.js
@@ -40,7 +40,7 @@ function createDataset() {
     dataset.addRow([pais.codigo, pais.nome, pais.sigla]);
   }
 
-  console.log("Dataset de países criado com sucesso.");
+  console.log("Dataset de países criado com sucesso. Total de países: " + paises.length);
 
   return dataset;
 }
